57dc64bf9dbd38c0284b0c407e9cecf9d4064337
Anishs1235 <150462659+Anishs1235@users.noreply.github.com>
2024-02-09 17:19:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+let soaps = document.querySelector("#soaps");
+let biscuits = document.querySelector("#biscuits");
+let grains = document.querySelector("#grains");
+let dresssoaps = document.querySelector("#dresssoaps");
